c5d1d2ce36c0995f6bf02e961404762bd0481c1d
Rodrigo López <rodrigohernanlopez89@gmail.com>
2023-01-25 13:34:04 -0300

    config validation fn

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -5,7 +5,18 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: (config) => {
+        /* console.log(config); */
+        if (!config.MONGO_URI)
+          throw new Error('Config variables not properly set');
+        return config;
+      },
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
