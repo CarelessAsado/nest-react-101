6bafdd71c4debb4db3bbbad5855d7f1b63b59b58
Rodrigo López <rodrigohernanlopez89@gmail.com>
2023-01-27 15:55:36 -0300

    decoding jwt auth guard

--- back/src/auth/guard/checkJWT.guard.ts
+++ back/src/auth/guard/checkJWT.guard.ts
@@ -1,8 +1,9 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Observable } from 'rxjs';
-import * as jwkToPem from 'jwk-to-pem';
-import { COGNITO_CONFIG } from 'src/constants';
 import { Request } from 'express';
+import * as jwkToPem from 'jwk-to-pem';
+import * as jwt from 'jsonwebtoken';
+import { COGNITO_CONFIG } from 'src/constants';
 
 let pems: { [key: string]: any } = {};
 
@@ -29,10 +30,41 @@ export class AuthGuard implements CanActivate {
     if (request.headers.auth) {
       console.log('JWT MAGIC WITH PEM');
       //decode
+      this.verifyToken(request);
       //check user and add to Context
     }
 
     return true;
+  }
+
+  verifyToken(req: Request) {
+    const headerToken = req.headers.auth as string;
+    console.log(headerToken);
+    if (!headerToken) return false;
+    //aca hago un decoding light, ver cual es la razon de esto
+    console.log(jwt, 77777777777777777777777);
+    let decodedJwt: any = jwt.decode(headerToken, { complete: true });
+    if (decodedJwt === null || decodedJwt.payload?.token_use !== 'access') {
+      return false;
+    }
+
+    console.log(decodedJwt, 'decodedJWT, light decoding');
+    let kid = decodedJwt.header.kid;
+    let pem = pems[kid];
+    console.log(pem, 'pem anda a saber q es');
+    if (!pem) {
+      return false;
+    }
+    jwt.verify(headerToken, pem, function (err: any, payload: any) {
+      if (err) {
+        return false;
+      } else {
+        console.log(payload, 'ver en q se diferencian los ID del access');
+        //Add user to the request object
+        /* req.user = payload.sub; */
+        return true;
+      }
+    });
   }
 
   private async setUp() {
